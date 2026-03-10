261db1daad40d94e0f9ccca69412224be03b49ab
honda4coding <mhndahmd820@gmail.com>
2026-03-10 20:34:56 +0200

    fix exam logic ref #4

--- js/Question.js
+++ js/Question.js
@@ -1,10 +1,11 @@
 class Question {
-    constructor({ id, text, options, correctAnswerIndex }) {
+    constructor( id, text, options, correctAnswerIndex ) {
         this.id = id;
         this.text = text;
         this.options = options;
         this.correctIndex = correctAnswerIndex;
         this.userAnswer = null; 
+        this.isMarked = false;
     }
 
     isCorrect() {
